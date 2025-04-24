7131cadb4f4656832caffbc8d823a33b606b3e8a
Luke Wengler <ldwengler@gmail.com>
2025-04-23 22:17:31 -0500

    added front folder

---
+++ front/src/main.ts
@@ -0,0 +1,6 @@
+const slider = document.querySelector(".range-slider");
+const progress = slider?.querySelector(".progress");
+const minPriceInput = slider?.querySelector(".min-price");
+const maxPriceInput = slider?.querySelector(".max-price");
+const minInput = slider?.querySelector(".min-input");
+const maxInput = slider?.querySelector(".max-input");
