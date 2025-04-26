011ed31cd7bd4f4cc9c5f058fa3d487fa39997a8
Luke Wengler <ldwengler@gmail.com>
2025-04-25 19:21:31 -0500

    thing

--- front/src/funcs.ts
+++ front/src/funcs.ts
@@ -1,3 +1,5 @@
+import * as d3 from 'd3'
+
 export function configureSliders(filterDiv: HTMLDivElement){
     const progress = filterDiv?.querySelector(".progress") as HTMLDivElement;
     // inputs boxes
