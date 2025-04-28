70eba15214134ae012a4d9d596292484cb541e39
Luke Wengler <ldwengler@gmail.com>
2025-04-27 21:40:53 -0500

    4/27

--- front/src/funcs.ts
+++ front/src/funcs.ts
@@ -1,4 +1,4 @@
-
+import * as d3 from 'd3'
 
 export function configureSliders(filterDiv: HTMLDivElement){
     const progress = filterDiv?.querySelector(".progress") as HTMLDivElement;
@@ -82,13 +82,41 @@ export async function makeRequest(data: RequestFormat){
         }
         return response.json();
     } catch (error){
-        console.log(error)
-        return 'failure'
+        console.log(error);
+        return 'failure';
     }
 }
 
 
 
-export function drawDataViz(objectiveData: Object){
+export function drawDataViz(rawObjectiveData: Object){
+    const objectiveData = (
+                        Object.entries(rawObjectiveData)
+                        .map(([objective, [mean, [lower, upper]]]) => 
+                            ({
+                            objective,
+                            mean,
+                            lower,
+                            upper
+                            })
+                        )
+    )
+    // redraw svg
+    const mainChartDiv = d3.select('#main-chart');
+    const oldSVG = mainChartDiv.select('svg')
+    if(oldSVG){
+        oldSVG.remove();
+    }
 
+    const margin = {top: 20, right: 40, bottom: 40, left: 120};
+    const width = 800 - margin.left - margin.right;
+    const height = objectiveData.length * 50; // 50px per row
+
+    const svgElement = mainChartDiv.append('svg')
+                        .attr('width', width + margin.left + margin.right)
+                        .attr("height", height + margin.top + margin.bottom)
+                        .append('g')
+                        .attr('transform', `translate(${margin.left},${margin.top})`)
+    
+    
 }
